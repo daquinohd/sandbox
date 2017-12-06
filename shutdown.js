327e0862baf64027fa5db39f990d832b206b9e39
Dion d'Aquino <daquinohd@users.noreply.github.com>
2017-12-06 16:43:22 -0500

    snippet - just in case

---
+++ shutdown.js
@@ -0,0 +1,26 @@
+window.addEventListener("load", function () {
+    $('body').prepend(
+    '<style>.shutdown-notice{background-color:#c74769;color:#fff;padding:15px 20px}a:hover{text-decoration:underline}</style><div class=shutdown-notice>Due to a lapse in government funding, cancer.gov sites and services will be unavailable until further notice.<br>Updates regarding government operating status and resumption of normal operations can be found at <a href=https://usa.gov>USA.gov</a></div>'
+    );
+});
+
+
+/**
+<style>
+    .shutdown-notice {
+        background-color: #c74769;
+        color: #ffffff;
+        padding: 15px 20px;
+    }
+    .shutdown-notice a {
+        color: #ffffff;            
+    }
+    .shutdown-notice a:hover {
+        text-decoration: underline;
+    }
+</style>
+<div class="shutdown-notice"> 
+    Due to a lapse in government funding, cancer.gov sites and services will be unavailable until further notice.<br/>
+    Updates regarding government operating status and resumption of normal operations can be found at <a href="https://usa.gov">USA.gov</a>
+</div>
+*/
