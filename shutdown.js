87aedbf762dadf887837191658d91335c91d6236
Dion d'Aquino <daquinohd@users.noreply.github.com>
2018-01-19 12:55:00 -0500

    Updated styles / text

--- shutdown.js
+++ shutdown.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function () {
     $('body').prepend(
-    '<style>.shutdown-notice{background-color:#c74769;color:#fff;padding:15px 20px}a:hover{text-decoration:underline}</style><div class=shutdown-notice>Due to a lapse in government funding, cancer.gov sites and services will be unavailable until further notice.<br>Updates regarding government operating status and resumption of normal operations can be found at <a href=https://usa.gov>USA.gov</a></div>'
+      '<style>.shutdown-notice{background:#bb0e3d;color:#fff;font-family:Arial,sans-serif;font-weight:700;font-size:1.125em;padding:15px 20px}.shutdown-notice a{color:#fff;text-decoration:underline}</style><div class=shutdown-notice>Because of a lapse in government funding, the information on this website may not be up to date, transactions submitted via the website may not be processed, and the agency may not be able to respond to inquiries until appropriations are enacted.<br>The NIH Clinical Center (the research hospital of NIH) is open. For more details about its operating status, please visit <a href=https://cc.nih.gov/ >cc.nih.gov</a>.<br>Updates regarding government operating status and resumption of normal operations can be found at <a href=https://usa.gov>USA.gov</a>.</div>'
     );
 });
 
@@ -8,19 +8,21 @@ window.addEventListener("load", function () {
 /**
 <style>
     .shutdown-notice {
-        background-color: #c74769;
-        color: #ffffff;
+        background: #bb0e3d;
+        color: white;
+        font-family: Arial,sans-serif;
+        font-weight: bold;
+        font-size: 1.125em;
         padding: 15px 20px;
     }
     .shutdown-notice a {
-        color: #ffffff;            
-    }
-    .shutdown-notice a:hover {
+        color: white;
         text-decoration: underline;
     }
 </style>
-<div class="shutdown-notice"> 
-    Due to a lapse in government funding, cancer.gov sites and services will be unavailable until further notice.<br/>
-    Updates regarding government operating status and resumption of normal operations can be found at <a href="https://usa.gov">USA.gov</a>
+<div class="shutdown-notice">
+    Because of a lapse in government funding, the information on this website may not be up to date, transactions submitted via the website may not be processed, and the agency may not be able to respond to inquiries until appropriations are enacted.<br/>
+    The NIH Clinical Center (the research hospital of NIH) is open. For more details about its operating status, please visit <a href="https://cc.nih.gov/">cc.nih.gov</a>.<br/>
+    Updates regarding government operating status and resumption of normal operations can be found at <a href="https://usa.gov">USA.gov</a>.
 </div>
 */
