4c5b8725d61df68252e24329b3ad5d92d8a6066e
Dion d'Aquino <daquinohd@users.noreply.github.com>
2018-01-19 13:23:39 -0500

    fixed p tags

--- shutdown.js
+++ shutdown.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function () {
     $('body').prepend(
-      '<style>.shutdown-notice{background:#bb0e3d;font-family:Arial,sans-serif;font-weight:700;font-size:17px;padding:15px 20px}.shutdown-notice a{color:#fff;text-decoration:underline}.shutdown-notice p{color:#fff}</style><div class=shutdown-notice><p>Because of a lapse in government funding, the information on this website may not be up to date, transactions submitted via the website may not be processed, and the agency may not be able to respond to inquiries until appropriations are enacted.<p><p>The NIH Clinical Center (the research hospital of NIH) is open. For more details about its operating status, please visit <a href=https://cc.nih.gov/ >cc.nih.gov</a>.<p><p>Updates regarding government operating status and resumption of normal operations can be found at <a href=https://usa.gov>USA.gov</a>.</div>'
+      '<style>.shutdown-notice{background:#bb0e3d;font-family:Arial,sans-serif;font-weight:700;font-size:17px;padding:15px 20px}.shutdown-notice a{color:#fff;text-decoration:underline}.shutdown-notice p{color:#fff}</style><div class=shutdown-notice><p>Because of a lapse in government funding, the information on this website may not be up to date, transactions submitted via the website may not be processed, and the agency may not be able to respond to inquiries until appropriations are enacted.<p>The NIH Clinical Center (the research hospital of NIH) is open. For more details about its operating status, please visit <a href=https://cc.nih.gov/ >cc.nih.gov</a>.<p>Updates regarding government operating status and resumption of normal operations can be found at <a href=https://usa.gov>USA.gov</a>.</div>'
     );
 });
 
@@ -22,8 +22,8 @@ window.addEventListener("load", function () {
     }
 </style>
 <div class="shutdown-notice">
-    <p>Because of a lapse in government funding, the information on this website may not be up to date, transactions submitted via the website may not be processed, and the agency may not be able to respond to inquiries until appropriations are enacted.<p/>
-    <p>The NIH Clinical Center (the research hospital of NIH) is open. For more details about its operating status, please visit <a href="https://cc.nih.gov/">cc.nih.gov</a>.<p/>
+    <p>Because of a lapse in government funding, the information on this website may not be up to date, transactions submitted via the website may not be processed, and the agency may not be able to respond to inquiries until appropriations are enacted.</p>
+    <p>The NIH Clinical Center (the research hospital of NIH) is open. For more details about its operating status, please visit <a href="https://cc.nih.gov/">cc.nih.gov</a>.</p>
     <p>Updates regarding government operating status and resumption of normal operations can be found at <a href="https://usa.gov">USA.gov</a>.</p>
 </div>
 */
